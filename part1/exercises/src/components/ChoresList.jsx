dfb3a0b2df773798fc3f8dfc218ba32651c8a672
June Lee <junelee@Junes-Laptop.local>
2024-03-20 23:22:38 -0500

    completed pt 1

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,16 @@
+import classes from './ChoresList.module.css';
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   let pageTitle = "Chores List";
+
+   return (
+      <div>
+          <u><h2 className={classes.choresHeading}>{pageTitle}</h2></u>
+          <i><ul className={classes.choresText}>
+            <li>GRADED 4</li>
+            <li>GRADED 5</li> 
+            <li>GRADED 6</li> 
+            <li>CATCH UP CHAPTERS</li>
+         </ul></i>
+      </div>
+   );
 }
